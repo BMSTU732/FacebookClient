47a931656ce022cd85b68d2d767e590e3aea3fee
Roman Petropavlov <roma24.petropavlov@gmail.com>
2016-11-02 16:58:05 +0300

    Update client.hpp

--- include/Facebook/client.hpp
+++ include/Facebook/client.hpp
@@ -1,3 +1,6 @@
+#ifndef VKCLIENT_CLIENT_HPP
+#define VKCLIENT_CLIENT_HPP
+
 #include <string>
 #include <map>
 
@@ -7,7 +10,13 @@ namespace Facebook
     {
     public:
         using dict_t = std::map<std::string, std::string>;
-        Client(dict_t settings);
+        Client(dict_t settings):_settings(settings) {}
         auto check_connection() -> bool;
+        auto get_friends() -> void;
+
+    private:
+        dict_t _settings;
+        static auto write_callback(char *data, size_t size, size_t nmemb, std::string &buff) -> size_t;
     };
 }
+#endif
