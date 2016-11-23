cd62cf247e76526e357790c405b0af654e0dc5dd
Roman Petropavlov <roma24.petropavlov@gmail.com>
2016-11-24 00:12:34 +0300

    Update client.hpp

--- include/Facebook/client.hpp
+++ include/Facebook/client.hpp
@@ -14,8 +14,6 @@ namespace Facebook {
 		FacebookClient() {};
 		FacebookClient(dict_t settings) : settings_(settings) {};
 		auto check_connection() -> bool;
-		auto friends_getAppUsers()->void;
-		static auto func(char* ptr, size_t size, size_t nmemb, std::string* link)->size_t;
 
 	private:
 		dict_t settings_;
