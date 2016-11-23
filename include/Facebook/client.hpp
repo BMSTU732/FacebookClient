ef082ee104ce8f992042e4b6694046888120ea94
Roman Petropavlov <roma24.petropavlov@gmail.com>
2016-11-24 00:06:22 +0300

    Update client.hpp

--- include/Facebook/client.hpp
+++ include/Facebook/client.hpp
@@ -1,22 +1,23 @@
 #ifndef FACEBOOKCLIENT_CLIENT_HPP
 #define FACEBOOKCLIENT_CLIENT_HPP
 
-#include <string>
 #include <map>
+#include <string>
+#include <iostream>
+#include <curl/curl.h>
 
-namespace Facebook
-{
-    class Client
-    {
-    public:
-        using dict_t = std::map<std::string, std::string>;
-        Client(dict_t settings):_settings(settings) {}
-        auto check_connection() -> bool;
-        \\auto get_friends() -> void;
+namespace Facebook {
+	class FacebookClient {
+	public:
+		using dict_t = std::map<std::string, std::string>;
 
-    private:
-        dict_t _settings;
-        \\static auto write_callback(char *data, size_t size, size_t nmemb, std::string &buff) -> size_t;
-    };
+		FacebookClient() {};
+		FacebookClient(dict_t settings) : settings_(settings) {};
+		auto check_connection() -> bool;
+		auto friends_getAppUsers()->void;
+		static auto func(char* ptr, size_t size, size_t nmemb, std::string* link)->size_t;
+
+	private:
+		dict_t settings_;
+	};
 }
-#endif
