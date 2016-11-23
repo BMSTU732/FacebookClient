84c452add9aa66fdf573fc20b923a6ed99ac7946
Roman Petropavlov <roma24.petropavlov@gmail.com>
2016-11-23 23:50:22 +0300

    Update client.hpp

--- include/Facebook/client.hpp
+++ include/Facebook/client.hpp
@@ -12,11 +12,11 @@ namespace Facebook
         using dict_t = std::map<std::string, std::string>;
         Client(dict_t settings):_settings(settings) {}
         auto check_connection() -> bool;
-        auto get_friends() -> void;
+        \\auto get_friends() -> void;
 
     private:
         dict_t _settings;
-        static auto write_callback(char *data, size_t size, size_t nmemb, std::string &buff) -> size_t;
+        \\static auto write_callback(char *data, size_t size, size_t nmemb, std::string &buff) -> size_t;
     };
 }
 #endif
